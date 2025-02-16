29d2fa4a788fedaafbb7b3d8126daa1b2b0dd6f8
Luis <luis@staudt-fridingen.de>
2025-02-16 19:28:56 +0100

    added read line method to string reader

--- src/main/java/net/luis/utils/io/data/yaml/YamlHelper.java
+++ src/main/java/net/luis/utils/io/data/yaml/YamlHelper.java
@@ -1,3 +1,21 @@
+/*
+ * LUtils
+ * Copyright (C) 2025 Luis Staudt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.luis.utils.io.data.yaml;
 
 import org.apache.commons.lang3.StringUtils;
@@ -16,6 +34,7 @@ final class YamlHelper {
 	
 	private static final Pattern COLON_WHITESPACE_PATTERN = Pattern.compile(":\\s");
 	private static final Pattern WHITESPACE_PATTERN = Pattern.compile("\\s");
+	static final String[] SPECIAL_CHARACTERS = new String[] { "*", "&", "!", ">", "|", "#", "@", "%", "[", "{" };
 	
 	private YamlHelper() {}
 	
@@ -24,7 +43,7 @@ final class YamlHelper {
 		if (key.isBlank()) {
 			throw new IllegalArgumentException("Key must not be empty or blank");
 		}
-		if (StringUtils.startsWithAny(key, "*", "&", "!", ">", "|", "#", "@", "%", "[", "{")) {
+		if (StringUtils.startsWithAny(key, SPECIAL_CHARACTERS)) {
 			throw new IllegalArgumentException("Key must not start with a special character (*, &, !, >, |, #, @, %, [, {)");
 		}
 		if (COLON_WHITESPACE_PATTERN.matcher(key).find()) {
